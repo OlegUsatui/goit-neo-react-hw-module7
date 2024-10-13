65f1da61c68ff4d48bf7fad7c881d92567362ebf
Oleh Usatyi <olegysik1@ukr.net>
2024-10-13 19:49:26 +0200

    deleted persist

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,13 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import {Provider} from "react-redux";
 import App from './App.jsx'
-import {store, persistor} from './redux/store';
+import {store} from './redux/store';
 import './index.css'
-import {PersistGate} from "redux-persist/integration/react";
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <Provider store={store}>
-            <PersistGate loading={null} persistor={persistor}>
-                <App/>
-            </PersistGate>
+            <App/>
         </Provider>
     </StrictMode>,
 )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,31 +1,12 @@
 import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import contactsReducer from './contactsSlice';
 import filtersReducer from './filtersSlice';
-import storage from 'redux-persist/lib/storage';
-import { persistReducer, persistStore } from 'redux-persist';
-
-const persistConfig = {
-    key: 'root',
-    storage,
-    whitelist: ['contacts'],
-};
 
 const rootReducer = combineReducers({
     contacts: contactsReducer,
     filters: filtersReducer,
 });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE', 'persist/FLUSH', 'persist/PAUSE', 'persist/PURGE', 'persist/REGISTER'],
-            },
-        }),
+    reducer: rootReducer,
 });
-
-
-export const persistor = persistStore(store);
